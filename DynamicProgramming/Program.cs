24b7a540cde9bb95c5de6012ffe84dc16a9785d1
mehedi <mehedisun@outlook.com>
2023-09-07 11:54:58 +0600

    last update

---
+++ DynamicProgramming/Program.cs
@@ -0,0 +1,34 @@
+﻿using static System.Console;
+
+
+// Static Binding
+OutputTimeUsingStaticBinding();
+
+
+// Dynamic Binding
+OutputTimeUsingDynamicBinding();
+
+GenerateRuntimeErrorUsingDynamicBinding();
+
+
+
+void OutputTimeUsingStaticBinding()
+{
+    DateTime dateTime = DateTime.Now;
+    string time = dateTime.ToLongTimeString();
+    WriteLine(time);
+}
+
+void OutputTimeUsingDynamicBinding()
+{
+    dynamic dateTime = DateTime.Now;
+    string time = dateTime.ToLongTimeString();
+    WriteLine(time);
+}
+
+void GenerateRuntimeErrorUsingDynamicBinding()
+{
+    dynamic dateTime = DateTime.Now;
+    string time = dateTime.IsCoffeeTime();
+    WriteLine(time);
+}
