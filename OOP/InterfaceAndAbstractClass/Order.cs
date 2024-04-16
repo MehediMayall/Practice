accaca4e11d5dda668958f6f6f63961561d8ddd9
mehedi <mehedisun@outlook.com>
2024-04-16 14:39:55 +0600

    last update

---
+++ OOP/InterfaceAndAbstractClass/Order.cs
@@ -0,0 +1,19 @@
+namespace InterfaceAndAbstractClass;
+
+public class Order : IList
+{
+    public void add()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void equals()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void get()
+    {
+        throw new NotImplementedException();
+    }
+}
