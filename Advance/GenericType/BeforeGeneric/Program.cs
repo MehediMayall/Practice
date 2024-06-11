1ef32110bc17943a3cba9851628abb456b906531
mehedi <mehedisun@outlook.com>
2024-06-11 22:08:18 +0600

    Developed problem without using generic

---
+++ Advance/GenericType/BeforeGeneric/Program.cs
@@ -0,0 +1,32 @@
+﻿using GenericType;
+
+Person p1 = new Person{
+    FirstName = "Mehedi",
+    LastName = "Hasan",
+    Age = 35,
+};
+
+Person p2 = new Person{
+    FirstName = "Virat",
+    LastName = "Kohli",
+    Age = 35,
+};
+
+
+swap(ref p1, ref p2);
+
+System.Console.WriteLine($"First Person Name: {p1.FirstName}");
+
+int x=10, y=20;
+swap(ref x,ref y);
+
+System.Console.WriteLine($"X: {x}, Y: {y}");
+
+// Boxing : converting primitive type to object type
+void swap(ref object first, ref object second)
+{
+    object temp = first;
+    first = second;
+    second = temp;
+}
+
---
+++ basic/practice/Program.cs
@@ -0,0 +1,42 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] arr = { 1, 2,4};
+        // WorkingDays.Monday = 3;
+        System.Console.WriteLine((int) Color.Green);
+
+        System.Console.WriteLine(new Person().id);
+
+        Point p;
+        p.X = 10;
+
+         
+    }
+
+    public class Person
+    {
+        public int id;
+    }
+
+    enum WorkingDays
+    {
+        Monday,
+        Tuesday
+    }
+    enum Color
+    {
+        Red,
+        Green=0,
+        Blue=0,
+    }
+
+    struct Point
+    {
+        private int x;
+        public int X{
+            get { return x; }
+            set { x = value; }
+        }
+    }
+}
