5125ebfd05a09f7802d253b0e03e8aafce3f4f23
mehedi <mehedisun@outlook.com>
2024-08-16 19:12:24 +0600

    Task Demo

---
+++ Asynchronous Programming/TaskDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿using static System.Console;
+
+
+// Creating worker thread using task
+Task task = new Task(printHelloWorld);
+task.Start();
+
+
+// Creating another worker thread that returns a value
+Task<string> taskThatReturns = new Task<string>(returnAStringValue);
+taskThatReturns.Start();
+taskThatReturns.Wait();
+
+WriteLine(taskThatReturns.Result);
+
+
+
+
+WriteLine("\nWaiting for user input to end");
+ReadLine();
+
+
+string returnAStringValue()
+{
+    Thread.Sleep(1000);
+    return $"\nThis is the return value from the thread {Thread.CurrentThread.ManagedThreadId}";
+}
+
+
+void printHelloWorld()
+{
+    WriteLine($"Worker Thread Id: {Thread.CurrentThread.ManagedThreadId}");
+    WriteLine("Hello World!\n");
+}
---
+++ Asynchronous Programming/ThreadJoin/Program.cs
@@ -0,0 +1,19 @@
+﻿using static System.Console;
+
+
+
+// Creating worker thread
+Thread th = new Thread(printHelloWorld);
+th.Start();
+
+// blocking and joining the main thread
+th.Join();
+WriteLine("Hello world printed");
+
+
+void printHelloWorld()
+{
+    WriteLine("Hello World!");
+    Thread.Sleep(2000);
+}
+
