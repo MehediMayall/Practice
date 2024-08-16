7511f3bf9bd50428a180bdeae56aaaec75470ca6
mehedi <mehedisun@outlook.com>
2024-08-16 19:24:39 +0600

    Task Factory

---
+++ Asynchronous Programming/TaskFactoryDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Net;
+using static System.Console;
+
+
+Task<string> task = Task.Factory.StartNew<string>(
+    () => GetPosts("https://jsonplaceholder.typicode.com/posts")
+);
+
+try
+{
+    WriteLine(task.Result);
+}
+catch(AggregateException ex){
+    WriteLine(ex.Message);
+}
+
+
+string GetPosts(string url)
+{
+    using var client = new WebClient();
+    return client.DownloadString(url);
+}
